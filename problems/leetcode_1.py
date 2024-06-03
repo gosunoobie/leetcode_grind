15c7d839816a5e1cab4201eb8965090470d58dfa
gosunoobie <raichholok@gmail.com>
2024-06-03 14:43:14 +0545

    try/ palindrome solution

--- problems/leetcode_1.py
+++ problems/leetcode_1.py
@@ -1,3 +1,13 @@
 # add two numbers
 
+class Solution:
+    def twoSum(self, nums , target):
+        prevMap = {} # val : index
+
+        for i, n in enumerate(nums):
+            diff = target -n
+            if diff in prevMap:
+                return [prevMap[diff] , i]
+            prevMap[n] = i
+        return 
 
---
+++ problems/leetcode_9.py
@@ -0,0 +1,6 @@
+class Solution:
+    def isPalindrome(self, x):
+        x = str(x)
+        rev = list(x)[::-1]
+        org = ''.join(rev) 
+        return org == x
