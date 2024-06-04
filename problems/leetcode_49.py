ed8bb339477cc409a44cb656f9b428fe7e2b5505
gosunoobie <raichholok@gmail.com>
2024-06-04 17:22:57 +0545

    sol/ group_anagrams

---
+++ problems/leetcode_49.py
@@ -0,0 +1,17 @@
+# Group anagrams
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs) :
+        res =  defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+            
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+
+            res[tuple(count)].append(s)  
+
+        return res.values()
+            
