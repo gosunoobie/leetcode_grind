11075adee19cd59de7abb5431fe569a76ed4eaac
gosunoobie <raichholok@gmail.com>
2024-06-04 17:50:08 +0545

    sol/ corrected solution

--- problems/leetcode_347.py
+++ problems/leetcode_347.py
@@ -4,11 +4,10 @@ class Solution:
     def topKFrequent(self, nums, k):
         num_dict = {}
         for num in nums:
-            if(num_dict.get(num)):
+            if num in num_dict:
                 num_dict[num] += 1
             else:
-                num_dict[num] = 0
-        res = list(num_dict.keys())
+                num_dict[num] = 1
+        sorted_dict = dict(sorted(num_dict.items(), key=lambda item: item[1], reverse=True))
+        res = list(sorted_dict.keys())
         return res[0:k]
-
-        
