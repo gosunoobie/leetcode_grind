b52991d31844e61fec018b11abe8c613e7436779
gosunoobie <raichholok@gmail.com>
2024-06-05 23:49:08 +0545

    cmt/ top k frequent

--- problems/leetcode_347.py
+++ problems/leetcode_347.py
@@ -12,21 +12,29 @@
 #        res = list(sorted_dict.keys())
 #        return res[0:k]
 
-class Solution:
-    def topKFrequent(self, nums, k):
-        count = {}
-        freq = [[] for i in range(len(nums) + 1)]
-        
-        for n in nums:
-            count[n] = 1 + count.get(n,0)
+from typing import List
 
-        for n,c in count.items():
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # Step 1: Count the frequency of each number in nums
+        count = {}
+        for n in nums:
+            count[n] = 1 + count.get(n, 0)
+
+        # Step 2: Create a list of lists to use as buckets for frequency
+        freq = [[] for i in range(len(nums) + 1)]
+        for n, c in count.items():
             freq[c].append(n)
 
+        # Step 3: Gather the top k frequent elements
         res = []
-
-        for i in range(len(freq) - 1, 0 , -1):
+        for i in range(len(freq) - 1, 0, -1):
             for n in freq[i]:
                 res.append(n)
                 if len(res) == k:
                     return res
+
+# Example usage
+solution = Solution()
+print(solution.topKFrequent([1,1,1,2,2,3], 2))  # Output: [1, 2]
+print(solution.topKFrequent([1], 1))  # Output: [1]
