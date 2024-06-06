a6e8bad4166691dea753c1b66a28c5196b9ad953
gosunoobie <raichholok@gmail.com>
2024-06-06 16:06:42 +0545

    sol/ longest consecutive sequence

---
+++ problems/leetcode_128.py
@@ -0,0 +1,14 @@
+#longestConsecutive - hard
+
+class Solution:
+    def longestConsecutive(self, nums):
+        numSet = set(nums)
+        longest = 0
+
+        for n in nums:
+            if (n-1) not in numSet:
+                length = 0
+                while( n + length) in numSet:
+                    length += 1
+                longest = max(length, longest)
+        return longest
