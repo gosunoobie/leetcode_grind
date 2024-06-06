5c87b04f84916ba9983c90a93539b2ce659e6c4a
gosunoobie <raichholok@gmail.com>
2024-06-06 14:28:22 +0545

    sol/ valid soduku comments

--- problems/leetcode_36.py
+++ problems/leetcode_36.py
@@ -1,14 +1,18 @@
 from collections import defaultdict
 class Solution:
     def isValidSudoku(self, board):
+        # setting the rows, cols, and squares to a dict of sets 
         rows = defaultdict(set)
         cols = defaultdict(set)
         squares = defaultdict(set)
+
         for r in range(9) :
             for c in range(9):
+                # skipping the . characters
                 if board[r][c] == '.':
                     continue
 
+                # if the number exists in the given row or col or squares then return False
                 if(board[r][c] in rows[r] or board[r][c] in cols[c] or board[r][c] in squares[(r//3, c//3)]):
                     return False
                 rows[r].add(board[r][c])
