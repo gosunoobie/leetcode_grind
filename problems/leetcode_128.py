c3f5271472f36857f79acbc34d0fcd6e6a82f102
gosunoobie <raichholok@gmail.com>
2024-06-06 16:08:25 +0545

    cmt/ longest consecutive sequence comments

--- problems/leetcode_128.py
+++ problems/leetcode_128.py
@@ -6,9 +6,13 @@ class Solution:
         longest = 0
 
         for n in nums:
+            # check if its the start of a sequence
             if (n-1) not in numSet:
                 length = 0
+            # check check until the end of the sequence
                 while( n + length) in numSet:
                     length += 1
+            # settings the longest if the new length is greater
                 longest = max(length, longest)
+
         return longest
