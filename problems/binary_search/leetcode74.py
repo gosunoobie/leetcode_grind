8c6f57f97a422a5d7f0958e4359789c24dc492ac
gosunoobie <raichholok@gmail.com>
2025-09-18 15:22:38 +1000

    2d search

--- problems/binary_search/leetcode74.py
+++ problems/binary_search/leetcode74.py
@@ -1,22 +1,28 @@
-class Solution(object):
-    def search(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        l, r = 0, len(nums) - 1
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        ROWS, COLS = len(matrix), len(matrix[0])
 
-        while l <= r:
+        top, bot = 0, ROWS - 1
+        while top <= bot:
+            row = (top + bot) // 2
+            if target > matrix[row][-1]:
+                top = row + 1 
+            elif target < matrix[row][0]:
+                bot = row - 1
+            else:
+                break
+        if not (top <= bot):
+            return False
+        
+        row = (top+bot)// 2
+        l, r = 0 , COLS - 1
+        while l <=r:
             m = (l+r)//2
-
-            if nums[m] < target :
+            if target > matrix[row][m]:
                 l = m + 1
-            elif nums[m] > target:
+            elif target < matrix[row][m]:
                 r = m - 1
             else:
-                return m 
-        return -1
+                return True
+        return False
 
-
- 
