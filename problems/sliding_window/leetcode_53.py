48ab68aeb8fe4d73eb3eeae369922d037faa53a9
gosunoobie <raichholok@gmail.com>
2026-08-21 23:34:29 +1000

    maximum-subarray-53

---
+++ problems/sliding_window/leetcode_53.py
@@ -0,0 +1,16 @@
+class Soltion(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        maxSub = nums[0]
+        curSum =0 
+
+        for n in nums:
+            if curSum < 0:
+                curSum =0
+            curSum += n
+            maxSub = max(maxSub, curSum)
+        return maxSub
+        
