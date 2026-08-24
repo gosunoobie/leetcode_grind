4fd6ce2a9d61481555f629ed0c7a12522a90ac21
gosunoobie <raichholok@gmail.com>
2026-08-24 23:53:30 +1000

    longest sub string

---
+++ problems/sliding_window/leetcode_3.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        seen = set()
+        l = 0
+        max_count = 0
+
+        for r in range(len(s)):
+            while s[r] in seen:
+                seen.remove(s[l])
+                l += 1
+
+            seen.add(s[r])
+            max_count = max(max_count, r - l + 1)
+
+        return max_count
